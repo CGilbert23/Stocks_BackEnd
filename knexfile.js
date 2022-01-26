49c5605ed4d90eb9f1fe3c312ef94fca7799660c
Chris Gilbert <christophergilbert20@gmail.com>
2022-01-26 13:19:21 -0500

    changes

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,12 @@
 
 const path = require("path");
 require("dotenv").config();
-const { DATABASE_URL } = process.env;
+
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: "postgres://sdqrpcsb:YmubJtExElxcrckFF4LjTfZbNOKR7viy@fanny.db.elephantsql.com/sdqrpcsb",
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
@@ -15,4 +15,14 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
+  production: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  }
 };
